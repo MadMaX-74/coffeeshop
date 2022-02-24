cbb886812c531f826894efef9bb7558f94feccfa
Maksim Maltcev <maksimmaltcev@MacBook-Pro-Maksim.local>
2022-02-24 21:37:52 +0300

    add form events

--- src/views/HeroView.vue
+++ src/views/HeroView.vue
@@ -1,67 +1,82 @@
 <template>
-    <main>
+  <main>
     <div class="preview">
-            <div class="container">
-                <div class="row">
-                    <div class="col">
-                        <nav-bar-component></nav-bar-component>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-10 offset-lg-1">
-                        <main-title title="Everything You Love About Coffee" />
-                        <img class="beanslogo" src="@/assets/logo/Beans_logo.svg" alt="Beans logo">
-                        <div class="preview__subtitle">We makes every day full of energy and taste</div>
-                        <div class="preview__subtitle">Want to try our beans?</div>
-                        <a href="./coffeepage.html" class="preview__btn">More</a>
-                    </div>
-                </div>
-            </div>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <nav-bar-component></nav-bar-component>
+          </div>
         </div>
-        <section class="about">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-6 offset-lg-3">
-                        <div class="title">About Us</div>
-                        <img class="beanslogo" src="@/assets/logo/Beans_logo_dark.svg" alt="Beans logo">
-                        <div class="about__text">
-                            Extremity sweetness difficult behaviour he of. On disposal of as landlord horrible.
-                            Afraid at highly months do things on at. Situation recommend objection do intention
-                            so questions. As greatly removed calling pleased improve an. Last ask him cold feel
-                            met spot shy want. Children me laughing we prospect answered followed. At it went
-                            is song that held help face.<br><br>
-
-                            Now residence dashwoods she excellent you. Shade being under his bed her, Much
-                            read on as draw. Blessing for ignorant exercise any yourself unpacked. Pleasant
-                            horrible but confined day end marriage. Eagerness furniture set preserved far
-                            recommend. Did even but nor are most gave hope. Secure active living depend son
-                            repair day ladies now.
-                        </div>
-                    </div>
-                </div>
+        <div class="row">
+          <div class="col-lg-10 offset-lg-1">
+            <main-title title="Everything You Love About Coffee" />
+            <img
+              class="beanslogo"
+              src="@/assets/logo/Beans_logo.svg"
+              alt="Beans logo"
+            />
+            <div class="preview__subtitle">
+              We makes every day full of energy and taste
             </div>
-        </section>
-        <section class="best">
-            <div class="container">
-                <div class="title">Our best</div>
-                <div class="row">
-                    <div class="col-lg-10 offset-lg-1">
-                        <div class="best__wrapper">
-                            <best-item-card
-                            v-for="bestseller in bestsellers"
-                            :key="bestseller.id"
-                            :name="bestseller.text"
-                            :price="bestseller.price"
-                            :image="bestseller.image"
-                            classItem = 'best__item'
-                             />
-                           
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+            <div class="preview__subtitle">Want to try our beans?</div>
+            <a
+              href="./coffeepage.html"
+              class="preview__btn"
+              @click.prevent="smoothScroll"
+              >More</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
+    <section class="about" id="about" ref="about">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 offset-lg-3">
+            <div class="title">About Us</div>
+            <img
+              class="beanslogo"
+              src="@/assets/logo/Beans_logo_dark.svg"
+              alt="Beans logo"
+            />
+            <div class="about__text">
+              Extremity sweetness difficult behaviour he of. On disposal of as
+              landlord horrible. Afraid at highly months do things on at.
+              Situation recommend objection do intention so questions. As
+              greatly removed calling pleased improve an. Last ask him cold feel
+              met spot shy want. Children me laughing we prospect answered
+              followed. At it went is song that held help face.<br /><br />
 
+              Now residence dashwoods she excellent you. Shade being under his
+              bed her, Much read on as draw. Blessing for ignorant exercise any
+              yourself unpacked. Pleasant horrible but confined day end
+              marriage. Eagerness furniture set preserved far recommend. Did
+              even but nor are most gave hope. Secure active living depend son
+              repair day ladies now.
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <section class="best">
+      <div class="container">
+        <div class="title" ref="ourBest">Our best</div>
+        <div class="row">
+          <div class="col-lg-10 offset-lg-1">
+            <div class="best__wrapper">
+              <best-item-card
+                v-for="bestseller in bestsellers"
+                :key="bestseller.id"
+                :name="bestseller.text"
+                :price="bestseller.price"
+                :image="bestseller.image"
+                classItem="best__item"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>
 
@@ -70,35 +85,44 @@ import NavBarComponent from "@/components/NavBarComponent.vue";
 import BestItemCard from "@/components/BestItemCard.vue";
 import MainTitle from "@/components/MainTitle.vue";
 
-    export default {
-        components: {NavBarComponent, BestItemCard, MainTitle},
-        data(){
-            return{
-                bestsellers: [
-                    {
-                        id: 0,
-                        image: 'coffee-1.jpg',
-                        text: 'Solimo Coffee Beans 2kg',
-                        price: 10.73
-                    },
-                    {
-                        id: 1,
-                        image: 'coffee-2.jpg',
-                        text: 'Presto Coffee Beans 1kg',
-                        price: 15.99
-                    },
-                    {
-                        id: 2,
-                        image: 'coffee-3.jpg',
-                        text: 'AROMISTICO Coffee 1kg',
-                        price: 6.99
-                    }
-                ]
-            }
+import { scrollIntoView } from "seamless-scroll-polyfill";
+
+export default {
+  components: { NavBarComponent, BestItemCard, MainTitle },
+  data() {
+    return {
+      bestsellers: [
+        {
+          id: 0,
+          image: "coffee-1.jpg",
+          text: "Solimo Coffee Beans 2kg",
+          price: 10.73,
         },
-    }
+        {
+          id: 1,
+          image: "coffee-2.jpg",
+          text: "Presto Coffee Beans 1kg",
+          price: 15.99,
+        },
+        {
+          id: 2,
+          image: "coffee-3.jpg",
+          text: "AROMISTICO Coffee 1kg",
+          price: 6.99,
+        },
+      ],
+    };
+  },
+  methods: {
+    smoothScroll() {
+      scrollIntoView(this.$refs.ourBest, {
+        behavior: "smooth",
+        block: "start",
+      });
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
